3c6cfb62e42a799bc9d25d96279b7fa638578c5c
Skander Chouba <skander.chouba@gmail.com>
2020-05-20 12:41:18 +0100

    fix1

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
