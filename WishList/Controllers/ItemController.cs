bed019825cda2b2dfdb1190b87bc791c4ce220b6
Skander Chouba <skander.chouba@gmail.com>
2020-05-20 13:26:56 +0100

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -38,9 +38,9 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            var item = _context.Items.SingleOrDefault(item => item.Id == Id);
+            var item = _context.Items.SingleOrDefault(item => item.Id ==id);
            
             
                 _context.Items.Remove(item);
